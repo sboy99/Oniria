da3b92ad19e0d768bd845fc15b534ecb23531ba2
Sagar Bera <contact.sagarbera@gmail.com>
2024-04-20 21:25:37 +0530

    feat(init): Oniria

---
+++ src/main/java/app/oniria/oniria/OniriaApplication.java
@@ -0,0 +1,13 @@
+package app.oniria.oniria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OniriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OniriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/oniria/oniria/OniriaApplicationTests.java
@@ -0,0 +1,13 @@
+package app.oniria.oniria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OniriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
